38fd6985fcb12b16928a3ce1f441de3e3a011230
Shama Hoque <shama.hoque@gmail.com>
2014-08-09 16:38:57 -0700

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ angular-phonegap-currentPosition-on-Map
 =======================================
 
 Angular service for showing live current position marker on Google Maps using Phonegap/Cordova
+
+Phonegap/Cordova Plugins used:
+- <a href="https://cordova.apache.org/docs/en/3.0.0/cordova_geolocation_geolocation.md.html">Geolocation </a> 
+
+Plugin Installation via Cordova CLI:
+$ cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-geolocation.git
+
+
+TO-DO:
+1) Add Example
+2) Tests
+
+
+
---
+++ currentPosition.js
@@ -0,0 +1,51 @@
+angular.module('example').factory('CurrentPosition', function($rootScope) {
+    return {
+        //call for registering to pause & resume service if app in background/foreground
+        init: function() {
+            var service = this;
+            document.addEventListener("pause", function() {
+                service.pause();
+            }, false);
+
+            document.addEventListener("resume", function() {
+                service.start();
+            }, false);
+
+        },
+        //call to start getting current position and broadcast it to intended controller
+        start: function() {
+            watchID = navigator.geolocation.watchPosition(function(position) {
+                console.log(position);
+                $rootScope.$broadcast('positionUpdate', position);
+            }, function(error) {
+                console.log(error);
+            }, {
+                enableHighAccuracy: true
+            });
+            store.set('watchID', watchID);
+            return watchID;
+        },
+        //call to stop getting current position
+        pause: function() {
+            watchID = store.get('watchID');
+            if (watchID != null) {
+                navigator.geolocation.clearWatch(watchID);
+                watchID = null;
+                store.set('watchID', watchID);
+            }
+        },
+        //call to add/update current position marker in the passed map at the specified position
+        addMarker: function(map, position) {
+            var currentLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            currentMarker = new google.maps.Marker({
+                map: map,
+                position: currentLatlng,
+                icon: 'img/bluedot.gif',
+                optimized: false
+            });
+
+            return currentMarker;
+        }
+    }
+});
